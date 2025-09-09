fc03e354e7570f73b0916531d06c1f1ca293e887
SY101605 <3488124829@qq.com>
2025-09-09 21:11:57 +0800

    commit lab4 successfully

--- lab4/flik/HorribleSteve.java
+++ lab4/flik/HorribleSteve.java
@@ -6,9 +6,10 @@ public class HorribleSteve {
         for (int j = 0; i < 500; ++i, ++j) {
             if (!Flik.isSameNumber(i, j)) {
                 throw new Exception(
-                        String.format("i:%d not same as j:%d ??", i, j));
+                       String.format("i:%d not same as j:%d ??", i, j));
             }
         }
         System.out.println("i is " + i);
     }
 }
+// 2的七次方等于128  128转换成二进制10000000
